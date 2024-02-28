5a42cfaeca0c29034797be4a6157a018c98625ea
Alessandro Silvestri <103107872+Alessandro-Silvestri@users.noreply.github.com>
2024-02-28 19:35:44 +0000

    Update C#_json_file_handle.cs

--- C#_json_file_handle.cs
+++ C#_json_file_handle.cs
@@ -21,12 +21,23 @@ namespace dir_files
                 'Comedy'
             ]
             }";
-
+            
+            ////////////////////////////////////////////////////////// using DeserializeObject() ///////////////////
             // creating the object using the class Movie with the method DeserializeObject()
             var jsonObj = JsonConvert.DeserializeObject<Movie>(json);
 
             // using the object for parsing
             Console.WriteLine(jsonObj.Genres[1]);
+
+            
+            ////////////////////////////////////////////////////////// using SerializeObject() ///////////////////
+            // here I can modify the object
+            var jsonObj = JsonConvert.DeserializeObject<Movie>(json);
+            jsonObj.Genres.Add("horror");
+            
+            // using SerializeObject() - converting the object in a string
+            string jsonOutput = JsonConvert.SerializeObject(jsonObj);
+            Console.WriteLine(jsonOutput);
         }
       
         // first create the class that contains the variable withe the same key names of the Json file
